9efd4c1ebae8182f4ddb17e57dda51518c8b6d81
Daamanmeet Paul <daamanmeet@gmail.com>
2017-04-03 15:35:46 -0700

    Gradient_descent implementation to adjust weights in order to get better accracy with minimum possible error

--- Gradient_descent/gradient_descent.py
+++ Gradient_descent/gradient_descent.py
@@ -1,54 +1,76 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  2 20:52:36 2017
+Created on Mon Apr  3 13:29:31 2017
 
 @author: Daamanmeet Paul
 """
+
 import numpy as np
+from data_prep import features, targets, features_test, targets_test
+
 
 def sigmoid(x):
     """
     Calculate sigmoid
     """
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
-def sigmoid_prime(x):
-    """
-    # Derivative of the sigmoid function
-    """
-    return sigmoid(x) * (1 - sigmoid(x))
+# TODO: We haven't provided the sigmoid_prime function like we did in
+#       the previous lesson to encourage you to come up with a more
+#       efficient solution. If you need a hint, check out the comments
+#       in solution.py from the previous lecture.
 
-learnrate = 0.5
-x = np.array([1, 2, 3, 4])
-y = np.array(0.5)
+# Use to same seed to make debugging easier
+np.random.seed(42)
 
-# Initial weights
-w = np.array([0.5, -0.5, 0.3, 0.1])
+n_records, n_features = features.shape
+last_loss = None
 
-### Calculate one gradient descent step for each weight
-### Note: Some steps have been consilated, so there are
-###       fewer variable names than in the above sample code
+# Initialize weights
+weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
 
-# TODO: Calculate the node's linear combination of inputs and weights
-h = np.matmul(x,w)
+# Neural Network hyperparameters
+epochs = 1000
+learnrate = 0.05
 
-# TODO: Calculate output of neural network
-nn_output = sigmoid(h)
+for e in range(epochs):
+    del_w = np.zeros(weights.shape)
+    for x, y in zip(features.values, targets):
+        # Loop through all records, x is the input, y is the target
 
-# TODO: Calculate error of neural network
-error = y - nn_output
+        # Note: We haven't included the h variable from the previous
+        #       lesson. You can add it if you want, or you can calculate
+        #       the h together with the output
 
-# TODO: Calculate the error term
-#       Remember, this requires the output gradient, which we haven't
-#       specifically added a variable for.
-error_term = error*sigmoid_prime(h)
+        # TODO: Calculate the output
+        output = sigmoid(np.matmul(weights,x))
 
-# TODO: Calculate change in weights
-del_w =learnrate*error_term*x
+        # TODO: Calculate the error
+        error = np.subtract(y,output)
 
-print('Neural Network output:')
-print(nn_output)
-print('Amount of Error:')
-print(error)
-print('Change in Weights:')
-print(del_w)
+        # TODO: Calculate the error term
+        error_term = error*((output)*(1-output))
+
+        # TODO: Calculate the change in weights for this sample
+        #       and add it to the total weight change
+        del_w += error_term*x
+
+    # TODO: Update weights using the learning rate and the average change in weights
+    weights += learnrate*del_w/n_records
+
+    # Printing out the mean square error on the training set
+    if e % (epochs / 10) == 0:
+        out = sigmoid(np.dot(features, weights))
+        loss = np.mean((out - targets) ** 2)
+        if last_loss and last_loss < loss:
+            print("Train loss: ", loss, "  WARNING - Loss Increasing")
+        else:
+            print("Train loss: ", loss)
+        last_loss = loss
+
+
+# Calculate accuracy on test data
+tes_out = sigmoid(np.dot(features_test, weights))
+predictions = tes_out > 0.5
+accuracy = np.mean(predictions == targets_test)
+print("Prediction accuracy: {:.3f}".format(accuracy))
